1eba445948ee9dd7d1cd298324fddda52b3ccc45
Raheel Junaid <60077374+raheelrjunaid@users.noreply.github.com>
2021-03-01 19:47:19 -0700

    Change csv file and merge multiple word definitions into one word

--- translator.py
+++ translator.py
@@ -7,12 +7,17 @@ print(f'[red]\n[u]Original[/u]:\n - {sentence}[/red]')
 # Convert string to list
 list_of_words = sentence.split()
 for word in list_of_words:
-    csv_file = open('shakespear_words_data.csv', 'r')
+    csv_file = open('./shakespeare_word_keys.csv', 'r')
     csv_data = csv.reader(csv_file)
 
     for data in csv_data:
         if word.lower() == data[0]:
-            list_of_words[list_of_words.index(word)] = data[1]
+            replacement_word = data[1]
+            if data[0] == 'thee' or data[0] == 'ye' or data[0] == 'thou':
+                replacement_word = 'you'
+            elif data[0] == 'thine' or data[0] == 'thy':
+                replacement_word = 'your'
+            list_of_words[list_of_words.index(word)] = replacement_word
             break
 
 # Convert list back to string
