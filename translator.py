b28d49481951e01c90069a75ce544001e42cb5ed
Raheel Junaid <60077374+raheelrjunaid@users.noreply.github.com>
2021-03-02 09:19:04 -0700

    Add punctuation mark sensing & comments

--- translator.py
+++ translator.py
@@ -5,22 +5,37 @@ sentence = input("Type a sentence: ")
 # Convert string to list
 list_of_words, strikethrough_list = sentence.split(), sentence.split()
 
+# Open csv file (needs to be opened multiple times to be read multiple times)
 csv_file = open('./shakespeare_word_keys.csv', 'r')
 csv_data = csv.reader(csv_file)
+
+# Strike out words that need to be removed
 for data in csv_data:
     if data[0] in list_of_words:
         strikethrough_list[list_of_words.index(data[0])] = f"[s]{data[0]}[/s]"
-sentence = " ".join(strikethrough_list)
 
+# Print new sentence with crossed out words
+sentence = " ".join(strikethrough_list)
 print(f'[red]\n[u]Original[/u]:\n - {sentence}[/red]')
 
 for word in list_of_words:
     csv_file = open('./shakespeare_word_keys.csv', 'r')
     csv_data = csv.reader(csv_file)
 
+    # Check for puntuation marks
+    if word[-1] == ",":
+        list_of_words[list_of_words.index(word)] = word.replace(",", "")
+        word = word.replace(",", "")
+    elif word[-1] == ".":
+        list_of_words[list_of_words.index(word)] = word.replace(".", "")
+        word = word.replace(".", "")
+
     for data in csv_data:
+        # See if word is in csv_data (is it Shakespearean)
         if word.lower() == data[0]:
             replacement_word = data[1]
+            # Change replacement word manually if there are multiple words
+            # Ex. Thee, ye, and thou all mean "you"
             if data[0] == 'thee' or data[0] == 'ye' or data[0] == 'thou':
                 replacement_word = 'you'
             elif data[0] == 'thine' or data[0] == 'thy':
