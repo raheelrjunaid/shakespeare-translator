2a9d70c41c7089a62aed5f7998caab1cdc8f8e28
Raheel Junaid <60077374+raheelrjunaid@users.noreply.github.com>
2021-03-02 16:40:00 -0700

    Add punctuation marks back in after removing + remove strikethough original

--- translator.py
+++ translator.py
@@ -3,22 +3,17 @@ from rich import print
 
 sentence = input("Type a sentence: ")
 # Convert string to list
-list_of_words, strikethrough_list = sentence.split(), sentence.split()
+list_of_words = sentence.split()
 
 # Open csv file (needs to be opened multiple times to be read multiple times)
 csv_file = open('./shakespeare_word_keys.csv', 'r')
 csv_data = csv.reader(csv_file)
 
-# Strike out words that need to be removed
-for data in csv_data:
-    if data[0] in list_of_words:
-        strikethrough_list[list_of_words.index(data[0])] = f"[s]{data[0]}[/s]"
-
-# Print new sentence with crossed out words
-sentence = " ".join(strikethrough_list)
+# Print original sentence
 print(f'[red]\n[u]Original[/u]:\n - {sentence}[/red]')
 
 for word in list_of_words:
+    punctuation_mark = ""
     csv_file = open('./shakespeare_word_keys.csv', 'r')
     csv_data = csv.reader(csv_file)
 
@@ -26,9 +21,11 @@ for word in list_of_words:
     if word[-1] == ",":
         list_of_words[list_of_words.index(word)] = word.replace(",", "")
         word = word.replace(",", "")
+        punctuation_mark = ","
     elif word[-1] == ".":
         list_of_words[list_of_words.index(word)] = word.replace(".", "")
         word = word.replace(".", "")
+        punctuation_mark = "."
 
     for data in csv_data:
         # See if word is in csv_data (is it Shakespearean)
@@ -41,8 +38,12 @@ for word in list_of_words:
             elif data[0] == 'thine' or data[0] == 'thy':
                 replacement_word = 'your'
             replacement_word = f"[bold u]{replacement_word}[/bold u]"
+            replacement_word += punctuation_mark
             list_of_words[list_of_words.index(word)] = replacement_word
             break
+    else:
+        replacement_word = word + punctuation_mark
+        list_of_words[list_of_words.index(word)] = replacement_word
 
 # Convert list back to string
 sentence = " ".join(list_of_words)
